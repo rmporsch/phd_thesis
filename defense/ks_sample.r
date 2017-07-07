0332f613efe6029c5eed5e2e8119236be1905400
Robert Porsch <rmporsch@gmail.com>
2017-07-07 13:10:51 +0800

    added plots for longherea as well as examples of the KS-Burden

---
+++ defense/ks_sample.r
@@ -0,0 +1,22 @@
+library(tidyverse)
+pp <- 1000
+n <- 500
+cas_p <- 0.0005
+con_p <- 0.0001
+clu_size <- 100
+clu_start <- 100
+
+con <- rbinom(pp,n, con_p)
+cas <- rbinom(pp,n, con_p)
+cas[clu_start:(clu_start+clu_size-1)] <- rbinom(clu_size, n, cas_p)
+dd <- data.frame("cases"=cas, "controls"=con, "position"=1:pp)
+
+example_plot <- dd %>%
+  mutate("cases"=cumsum(cases)/sum(cases), "controls"=cumsum(controls)/sum(controls)) %>%
+  gather("Group", "value", -position) %>%
+  ggplot(., aes(y=value, x=position, color=Group)) +
+  geom_step() +
+  ylab("Cumulative Probability") +
+  theme_classic()
+
+#ggsave("./plots/ks_example.pdf", example_plot)
